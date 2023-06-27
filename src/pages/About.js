cfe5c1e738a805f3f97398200a19a6abcbf9d950
codesieg <sieg.heart@hotmail.fr>
2023-06-27 09:40:07 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, labore magnam itaque asperiores quaerat in. Aperiam, sint dicta sed accusantium quis ducimus illum deleniti natus pariatur possimus, architecto perferendis a doloribus adipisci voluptate. Impedit expedita vero a iste ad excepturi assumenda, architecto modi recusandae praesentium magni. Vel eos maxime deserunt ullam alias accusamus amet aut, explicabo unde officia aspernatur porro non reprehenderit eum omnis dolore praesentium accusantium voluptates recusandae, ipsam ipsum odio similique ab nostrum. In aliquid velit sit. Dolorum laudantium in maxime perferendis, dicta impedit eum explicabo, quia ducimus tempore neque et earum repellendus sunt? Minima laudantium doloribus ipsam.</p>
+        </div>
+    );
+};
+
+export default About;
